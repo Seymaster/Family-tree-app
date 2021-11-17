69d41b0a4f04a50325c2d4cdc01148b12ccc51ff
Seymaster <alugbinoluwaseyi1@gmail.com>
2021-11-17 04:53:11 +0100

    user instead

--- controllers/webhook.js
+++ controllers/webhook.js
@@ -1,10 +1,9 @@
 exports.getNewUser = async (req,res,next) =>{
     let user = req.body
-    user = JSON.parse(user)
     console.log(user)
     return res.status(200).send({
         status: 200,
-        message: "Webhook returned successfully",
-        data: user
+        message: "User Created successfully",
+        data: data
     })
 }
